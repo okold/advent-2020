335c75fe56fe192c53396304ebbc10f3663aad0e
Olga <o.koldachenko@gmail.com>
2020-12-05 21:25:40 -0700

    Day 5

---
+++ 05/ticket.py
@@ -0,0 +1,49 @@
+# Advent of Code 2020
+# December 05, 2020
+# https://adventofcode.com/2020/day/5
+import func
+
+# gen_all_ids()
+def gen_all_ids():
+    list = []
+    for row in range(0, 127):
+        for col in range(0, 8):
+            list.append(row * 8 + col)
+    return list
+
+input = func.file_to_list("05/input")
+max_id = 0
+all_ids = gen_all_ids()
+
+for line in input:
+    max_row = 127
+    min_row = 0
+    max_col = 7
+    min_col = 0
+    
+    for char in line:
+        if char == 'F':
+            max_row = func.get_mid(max_row, min_row)
+        if char == 'B':
+            min_row = func.get_mid(max_row, min_row)
+        if char == 'L':
+            max_col = func.get_mid(max_col, min_col)
+        if char == 'R':
+            min_col = func.get_mid(max_col, min_col)
+    
+    # PART 1
+    seat_id = max_row * 8 + max_col
+    if seat_id > max_id:
+        max_id = seat_id
+
+    #PART 2
+    all_ids.remove(seat_id)
+
+for i, val in enumerate(all_ids):
+    if i > 0 and i < max_id:
+        if all_ids[i-1] != (val - 1) and all_ids[i+1] != (val + 1):
+            my_id = val
+            break
+
+print("PART 1:", max_id)
+print("PART 1:", my_id)
--- README.md
+++ README.md
@@ -4,4 +4,5 @@ I'm using this to learn Python, basically from scratch.
 - Day 1: Used a read_integers function found online to create a list from the input, not fully understanding lists and functions in this language yet.
 - Day 2: Getting more comfortable with string functions.
 - Day 3: Started creating functions from scratch, learned about enumerate(). Considering making a library.
-- Day 4: Things are getting more complex. Fighting the syntax. Created a module for list ~~functions~~ methods. Discovered the regular expression library. 
+- Day 4: Things are getting more complex. Fighting the syntax. Created a module for list ~~functions~~ methods. Discovered the regular expression library.
+- Day 5: Disappointed that Python doesn't have switch statements and arrays. I have to stop thinking of these programs in Java terms and start thinking more like I'm using Prolog or Haskell. I can see how it fits a niche, but it's definitely not a language I'm going to be using for large projects.
